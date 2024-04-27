393171117945f7823b3a37ee39ede9a4ba0c6648
AleLuppi <alelup@yahoo.it>
2024-04-27 23:11:30 +0200

    Introduce custom button that allows outline color
    Extend Quasar button by allowing setting a custom outline color, different from text color.
    Also introduce interfaces for base components' props.

---
+++ frontend/src/components/base/ABtn.vue
@@ -0,0 +1,25 @@
+<template>
+  <q-btn v-bind="$props" class="a-btn" />
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+import { colors } from 'quasar';
+import type { ABtnProps } from './baseComponents';
+
+// Define props
+const props = withDefaults(defineProps<ABtnProps>(), {
+  padding: 'sm',
+});
+
+// Get outline color
+const outlineHexColor = computed(() =>
+  props.outlineColor ? colors.getPaletteColor(props.outlineColor) : undefined,
+);
+</script>
+
+<style scoped lang="scss">
+.a-btn.q-btn--outline:before {
+  color: v-bind('outlineHexColor');
+}
+</style>
--- frontend/src/components/base/ABtnDark.vue
+++ frontend/src/components/base/ABtnDark.vue
@@ -1,22 +1,19 @@
 <template>
-  <q-btn
+  <a-btn
     v-bind="$props"
     :icon="isDark ? fasMoon : fasSun"
-    class="a-btn-dark"
     @click="isDark = !isDark"
   />
 </template>
 
 <script setup lang="ts">
 import { computed } from 'vue';
-import { colors, useQuasar } from 'quasar';
-import type { QBtnProps } from 'quasar';
+import { useQuasar } from 'quasar';
+import type { ABtnDarkProps } from './baseComponents';
 import { fasMoon, fasSun } from '@quasar/extras/fontawesome-v6';
 
 // Define props
-const props = defineProps<
-  Omit<QBtnProps, 'icon'> & { outlineColor?: string }
->();
+defineProps<ABtnDarkProps>();
 
 // Init plugin
 const $q = useQuasar();
@@ -26,15 +23,4 @@ const isDark = computed({
   get: () => $q.dark.isActive,
   set: (val) => $q.dark.set(val),
 });
-
-// Get outline color
-const outlineHexColor = computed(() =>
-  props.outlineColor ? colors.getPaletteColor(props.outlineColor) : undefined,
-);
 </script>
-
-<style scoped lang="scss">
-.a-btn-dark.q-btn--outline:before {
-  color: v-bind('outlineHexColor');
-}
-</style>
