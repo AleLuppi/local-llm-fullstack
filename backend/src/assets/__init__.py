9cbca1cec72551ad75e5b768e96681b64b27ed09
AleLuppi <alelup@yahoo.it>
2024-05-05 21:50:51 +0200

    Introduce methods for general purpose LLM chat
    Create LLM chain and methods for general purpose chatting.
    Note: this requires GPT4All Mistral 7b model in "assets" folder. Will be added to documentation.

---
+++ backend/src/assets/__init__.py
@@ -0,0 +1,17 @@
+from os.path import abspath, join as path_join, dirname
+
+
+def resolve(*paths: str):
+    """
+    Resolve a path relative to the "assets" directory.
+
+    :param paths: path components to join.
+    :return: absolute path resolved to the "assets" directory.
+    """
+    # Base "assets" directory name
+    __DIR_NAME = dirname(__file__)
+
+    return abspath(path_join(__DIR_NAME, *paths))
+
+
+LLM_MODEL_MISTRAL_7B = resolve("mistral-7b-openorca.gguf2.Q4_0.gguf")
