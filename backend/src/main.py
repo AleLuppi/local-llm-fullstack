37c2b21212378baa21665067727c962e44b9bf4f
AleLuppi <alelup@yahoo.it>
2024-07-08 00:42:20 +0200

    Remove console of bundled server app
    Disable uvicorn logging to avoid exceptions in bundled windowed app.
    Update requirements to include pyinstaller and updated uvicorn package.

--- backend/src/main.py
+++ backend/src/main.py
@@ -4,6 +4,7 @@ Start ASGI server to serve FastAPI application.
 
 from multiprocessing import freeze_support
 from uvicorn import run as uvicorn_run
+from uvicorn.config import LOGGING_CONFIG
 
 from config import CONFIG
 
@@ -12,4 +13,5 @@ if __name__ == '__main__':
     freeze_support()
 
     api_port = int(CONFIG['APP_API_PORT'])
-    uvicorn_run(r"api.app:app", host="0.0.0.0", port=api_port, reload=False, workers=1)
+    uvicorn_run(r"api.app:app", host="0.0.0.0", port=api_port, reload=False, workers=1,
+                log_config=LOGGING_CONFIG if not CONFIG['IS_BUNDLED'] else None)
